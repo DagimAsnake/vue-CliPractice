34edd315ae92058d86eb62bfc4aaac6e70f92657
Dagim Asnake <dagimasnake1@gmail.com>
2023-09-15 10:12:58 -1200

    first vue cli app

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsVisible: false,
+      friend: {
+        id: "1",
+        name: "dagim",
+        phone: "0943439692",
+        email: "dagim@gamil.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsVisible = !this.detailsVisible;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
