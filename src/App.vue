9af0cc20af7caad216fca487f8787ffc54d8a0d6
Dagim Asnake <dagimasnake1@gmail.com>
2023-09-16 06:02:28 -1200

    finished first vue cli app

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,69 @@
 <template>
- <section>
+  <section>
     <header>
       <h1>My Friends</h1>
     </header>
+    <new-friend @add-contact="addFriend"></new-friend>
     <ul>
-      <friend-contact></friend-contact>
-      <friend-contact></friend-contact>
+      <friend-contact
+        v-for="friend in friends"
+        :key="friend.id"
+        :id="friend.id"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+        :is-favorite="friend.isFavorite"
+        @toggle-favorite="toggleFavoriteStatus"
+        @delete="deleteContact"
+      ></friend-contact>
     </ul>
   </section>
 </template>
 
 <script>
+import NewFriend from './components/NewFriend.vue';
 export default {
-    data() {
-        return {
-            friends: [
-                {
-                    id: '1',
-                    name: 'dagim',
-                    phone: '0943439692',
-                    email: 'dagim@gamil.com'
-                },
-                 {
-                    id: '2',
-                    name: 'abel',
-                    phone: '0969485464',
-                    email: 'Abel@gamil.com'
-                },
-            ]
-        }
+  components: { NewFriend },
+  data() {
+    return {
+      friends: [
+        {
+          id: "1",
+          name: "dagim",
+          phone: "0943439692",
+          email: "dagim@gamil.com",
+          isFavorite: true
+        },
+        {
+          id: "2",
+          name: "abel",
+          phone: "0969485464",
+          email: "Abel@gamil.com",
+          isFavorite: false
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleFavoriteStatus(friendId) {
+      const identifiedFriend = this.friends.find(friend => friend.id === friendId)
+      identifiedFriend.isFavorite = !identifiedFriend.isFavorite
+    },
+    addFriend(name, phone, email) {
+      const newFriendContact = {
+        id: new Date().toISOString,
+        name: name,
+        phone: phone,
+        email: email,
+        isFavorite: false
+      }
+      this.friends.push(newFriendContact)
+    },
+    deleteContact(friendId) {
+      this.friends = this.friends.filter(friend => friend.id !== friendId)
     }
-}
+  }
+};
 </script>
 
 <style>
@@ -38,7 +71,7 @@ export default {
   box-sizing: border-box;
 }
 html {
-  font-family: "Jost", sans-serif;
+  font-family: 'Jost', sans-serif;
 }
 body {
   margin: 0;
@@ -59,7 +92,8 @@ header {
   padding: 0;
   list-style: none;
 }
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -88,5 +122,18 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,30 +1,67 @@
 <template>
   <li>
-    <h2>{{ friend.name }}</h2>
-    <button @click="toggleDetails">Show Details</button>
+    <h2>{{ name }} {{ isFavorite ? "(favorite)" : "" }}</h2>
+    <button @click="toggleDetails">{{ detailsVisible ? "Hide Details" : "show Details"}}</button>
+    <button @click="toggleFavorite">Show Favorite</button>
     <ul v-if="detailsVisible">
-      <li><strong>Phone:</strong>{{ friend.phone }}</li>
-      <li><strong>Email:</strong>{{ friend.email }}</li>
+      <li><strong>Phone:</strong>{{ phoneNumber }}</li>
+      <li><strong>Email:</strong>{{ emailAddress }}</li>
     </ul>
+    <button @click="$emit('delete', id)">Delete</button>
   </li>
 </template>
 
 <script>
 export default {
+  // props: ["name", "emailAddress", "phoneNumber", "isFavorite"],
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: true,
+      // validator: function(value) {
+      //   return value === '1' || value === '0'
+      // }
+    },
+  },
+  emits: ['toggle-favorite', 'delete'],
+  // emits: {
+  //   "toggle-favorite": function (id) {
+  //     if (id) {
+  //       return true;
+  //     } else {
+  //       console.warn("Id is missing..");
+  //       return false;
+  //     }
+  //   },
+  // },
   data() {
     return {
       detailsVisible: false,
-      friend: {
-        id: "1",
-        name: "dagim",
-        phone: "0943439692",
-        email: "dagim@gamil.com",
-      },
     };
   },
   methods: {
     toggleDetails() {
       this.detailsVisible = !this.detailsVisible;
+    },
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id);
     },
   },
 };
